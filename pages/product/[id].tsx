ddcff763fb4a1117d4b2141b23caf2c1f7d0d1d3
srji <iosergio.valbuena@gmail.com>
2022-06-29 13:47:01 -0700

    server site render nextJS features added

--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,24 +1,45 @@
 import React, { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import fetch from 'isomorphic-unfetch'
+import { GetStaticProps } from 'next'
 
 import Layout from '@components/Layout/Layout'
 import ProductSummary from '@components/ProductSummary/ProductSummary'
 
-const ProductPage = () => {
-  const { query } = useRouter()
-  const [product, setProduct] = useState<TProduct | null>(null)
-
-  useEffect(() => {
-    if (query.id) {
-      window
-        .fetch(`/api/avo/${query.id}`)
-        .then((response) => response.json())
-        .then((data: TProduct) => {
-          setProduct(data)
-        })
+//esta es una paguna Dinamica. 
+//entonces debemos decirle cuales son todas las paginas 
+export const getStaticPaths = async () => { 
+  const response = await fetch('https://avoapp.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
+  
+  const paths = productList.map(({id}) => ({
+    params: {
+      id
     }
-  }, [query.id])
+  }))
 
+  return {
+    paths,
+    //incremntal static generation
+    //cada pagina que no este incluida en el path dara un 404
+    fallback: false
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.id as string
+  const response = await fetch(`https://avoapp.vercel.app/api/avo/${id}`)
+  const product : TProduct = await response.json()
+
+  return {
+    props: {
+      product,
+    },
+  }
+}
+
+const ProductPage = ({ product }: {product: TProduct}) => {
+  
   return (
     <Layout>
       {product == null ? null : <ProductSummary product={product} />}
